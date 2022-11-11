db413d39e56adfd264c82a6197bdd8dda3acccb5
bl_liu <111820499+bl-liu6@users.noreply.github.com>
2022-11-11 12:59:30 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,15 +8,24 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClassClick />
+        <NameList />
+        {/* <UserGreeting /> */}
+        {/* <ParentComponent /> */}
+        {/* <EventBind /> */}
+        {/* <ClassClick />
         <FunctionClick />
         <Counter />
         <Message />
+
         <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
@@ -24,11 +33,11 @@ class App extends Component {
           <button>Action</button>
         </Greet>
         <Greet name="Diana" heroName="Wonderwoman" />
-        {/*Welcome component is a class component*/}
+        
         <Welcome name="Bruce" heroName="Batman" />
         <Welcome name="Clark" heroName="Superman" />
         <Welcome name="Diana" heroName="Wonderwoman" />
-        <Hello />
+        <Hello /> */}
       </div>
     )
   }
